5265dad69cfa3dfa848165eef96a3660cf0bcc01
Kamrul Hasan <113679107+Kamrul-Hasan-2@users.noreply.github.com>
2023-09-25 00:26:26 +0600

    sieve

---
+++ PrimeSieve.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+//#define n 100000
+
+void PrimeSieve(int n){
+    int prime[n] = {0};
+
+    for(int i = 2; i<=n; i++){
+        if(prime[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                prime[j]=1;
+            }
+        }
+    }
+
+    for(int i=2; i<=n; i++){
+        if(prime[i]==0){
+            cout << i << " ";
+        }
+    }
+
+}
+
+int main(){
+    int k;
+    cin >> k;
+    PrimeSieve(k);
+}
